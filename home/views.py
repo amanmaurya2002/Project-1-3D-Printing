774040a43f91ab730791b270685fe4dac8ebad69
Aman Maurya <amanmaurya2002@gmail.com>
2021-07-14 14:44:13 +0530

    Description not neede for now

--- home/views.py
+++ home/views.py
@@ -26,20 +26,8 @@ def contact(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         phone = request.POST.get('phone')
-        desc = request.POST.get('desc')
-        contact = Contact(name = name , email=email , phone=phone , desc=desc , date=datetime.today())
+        contact = Contact(name = name , email=email , phone=phone, date=datetime.today())
         contact.save()
 
     
     return render(request , 'contact.html')
-
-
-
-
-
-
-    
-    
-    
-    
-
