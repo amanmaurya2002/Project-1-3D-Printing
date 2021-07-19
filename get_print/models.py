26f25eab92ff1d30f80ffb5db289db66b99c5c1f
Aman Maurya <amanmaurya2002@gmail.com>
2021-07-19 22:06:37 +0530

    Consumer model auto creation on User creation

--- get_print/models.py
+++ get_print/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -11,3 +13,10 @@ class Part(models.Model):
 class Consumer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     parts = models.ManyToManyField(Part, blank=True)
+
+@receiver(post_save, sender=User)
+def update_consumer(sender, instance, created, **kwargs):
+    if created:
+        Consumer.objects.create(user=instance)
+    else:
+        instance.consumer.save()
